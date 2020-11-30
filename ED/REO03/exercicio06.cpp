0ecb1e03e76c963ddcd1003d3cce8d75ac796c50
Jose Victor A <jose.morais1@estudante.ufla.br>
2020-11-29 22:26:44 -0300

    EX 06 nota 37 no dredd, mas funcionando

--- ED/REO03/exercicio06.cpp
+++ ED/REO03/exercicio06.cpp
@@ -1,3 +1,6 @@
+//No método ::info(), deixei comentado outras 2 opções de impressão da fila/vetor para fins de depuração
+//e correção manual
+
 #include <iostream>
 using namespace std;
 
@@ -32,12 +35,12 @@ circularQueue::~circularQueue(){
 
 void circularQueue::enqueue(int value){
     if(mSize < capacity){
-        tail++;
-        tail = tail % capacity;
-        data[tail] = value;
         if(empty()){
             head++;
         }
+        tail++;
+        tail = tail % capacity;
+        data[tail] = value;
         mSize++;
     }
 }
@@ -64,20 +67,57 @@ bool circularQueue::empty(){
 }
 
 void circularQueue::info(){
+    //Esse método faz quebra de estrutura, entretanto pedido pelo exercicio.
     cout << "tamanho=" << mSize
     << " capacidade=" << capacity
     << " inicio=" << head
     << " fim=" << tail << endl;
 
-    // Como explicado na live do dia 25/11, saida na ordem do vetor.
-    if(mSize == 1){ //Se o vetor só tiver um dado, mostra apenas o dado e a posição dele no vetor.
+    // Como explicado na live do dia 25/11, impressao na ordem do vetor.
+
+    //Impressão na ordem do vetor, sem saída de lixo
+    if(not empty()){
+        if(tail>=head){
+            for(int i = head; i <= tail; i++){
+                cout << data[i] << " ";
+            }
+        }else{
+            for(int i = 0; i <= tail; i++){
+                cout << data[i] << " ";
+            }
+            for(unsigned i = head; i < capacity; i++){
+                cout << data[i] << " ";
+            }
+        }
+    }else{ //não há dados no vetor que não sejam lixo, portanto não dá a saida.
+        cout << "";
+    }
+
+    //Impressão na ordem do vetor, com saida de lixo.
+    /*if(mSize == 1){ //Se o vetor só tiver um dado, mostra apenas o dado e a posição dele no vetor.
         cout << data[head];
     }else{ 
         for(unsigned i = 0; i < capacity; i++){
-            cout << data[i] << " "; //Quebra de estrutura, porem pedido pelo exercício
+            cout << data[i] << " "; 
         }
-    }
+    }*/
 
+
+    // Impressão na ordem da fila.
+    /*if(mSize != 0){
+        if(tail>=head){
+        for(int i = head; i <= tail; i++){
+                cout << data[i] << " ";
+            }
+        }else{
+            for(int i = head; i < mSize; i++){
+                cout << data[i] << " ";
+            }
+            for(int i = 0; i <= tail; i++){
+                cout << data[i] << " ";
+            }
+        }
+    } */
     cout << endl;
 }
 
@@ -107,19 +147,3 @@ int main(){
 
     return 0;
 }
-
-// saida na ordem da fila 
-    /*if(mSize != 0){
-        if(tail>=head){
-        for(int i = head; i <= tail; i++){
-                cout << data[i] << " ";
-            }
-        }else{
-            for(int i = head; i < mSize; i++){
-                cout << data[i] << " ";
-            }
-            for(int i = 0; i <= tail; i++){
-                cout << data[i] << " ";
-            }
-        }
-    } */
